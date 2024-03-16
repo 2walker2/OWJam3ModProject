63f4a08d52cb6dfbf4fe81268ddaee899910e65c
mcdon546 <48959058+2walker2@users.noreply.github.com>
2024-03-16 01:14:53 -0400

    Added a debug warp script

---
+++ OWJam3ModProject/DebugWarp.cs
@@ -0,0 +1,30 @@
+﻿using OWML.Common;
+using OWML.ModHelper;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace OWJam3ModProject
+{
+    internal class DebugWarp : MonoBehaviour
+    {
+        [Tooltip("The control path for the key you must be holding to activate this warp")]
+        [SerializeField] Key holdKey = Key.W;
+        [Tooltip("The control path for the key you press to activate this wap")]
+        [SerializeField] Key pressKey = Key.Digit1;
+
+        void Update()
+        {
+            //Warp the player to this transform when a key combination is pressed
+            if (Keyboard.current[holdKey].IsPressed() && Keyboard.current[pressKey].wasPressedThisFrame)
+            {
+                OWRigidbody playerBody = Locator.GetPlayerBody();
+                if (playerBody != null)
+                {
+                    playerBody.SetPosition(transform.position);
+                    playerBody.SetVelocity(Vector3.zero);
+                    playerBody.SetAngularVelocity(Vector3.zero);
+                }
+            }
+        }
+    }
+}
